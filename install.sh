34be5d7db68f29d2a5e18ed8e0bbf33250e83b81
Capybara-z <stas89091000@yandex.ru>
2025-05-10 01:43:41 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
 
-NC='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-BOLD='\033[1m'
-BOLD_GREEN='\033[1;32m'
-BOLD_YELLOW='\033[1;33m'
-BOLD_RED='\033[1;31m'
-
-info() {
-    echo -e "${BOLD_BLUE}[INFO]${NC} $1"
-}
-
-warn() {
-    echo -e "${BOLD_YELLOW}[WARN]${NC} $1"
-}
-
-error() {
-    echo -e "${BOLD_RED}[ERROR]${NC} $1"
-}
-
-success() {
-    echo -e "${BOLD_GREEN}[SUCCESS]${NC} $1"
-}
-
-question() {
-    echo -e "${BOLD_CYAN}[QUESTION]${NC} $1"
-}
+source "$(dirname "$0")/scripts/common/functions.sh"
 
 TEMP_DIR=$(mktemp -d)
 cd "$TEMP_DIR" || exit 1
