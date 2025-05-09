60402c93b1f1986b52833ac6e8dc9d355259e0ab
Capybara-z <stas89091000@yandex.ru>
2025-05-10 02:28:45 +0300

    Update

--- scripts/remnanode/install-bbr.sh
+++ scripts/remnanode/install-bbr.sh
@@ -4,27 +4,30 @@ source "$(dirname "$0")/../../scripts/common/colors.sh"
 source "$(dirname "$0")/../../scripts/common/functions.sh"
 
 check_bbr() {
-    if sysctl net.ipv4.tcp_congestion_control | grep -q bbr; then
+    if sysctl net.ipv4.tcp_congestion_control | grep -q "bbr"; then
         info "BBR уже настроен"
         read -n 1 -s -r -p "Нажмите любую клавишу для возврата в меню..."
-        "$(dirname "$0")/../../scripts/remnanode/menu.sh"
-        exit 0
+        "$(dirname "$0")/menu.sh"
     fi
+    return 0
 }
 
 install_bbr() {
-    info "Настройка TCP BBR..."
-    sudo sh -c 'modprobe tcp_bbr && sysctl net.ipv4.tcp_available_congestion_control && sysctl -w net.ipv4.tcp_congestion_control=bbr && echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf && sysctl -p'
-    success "BBR успешно настроен!"
+    info "Установка BBR..."
+    echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -p
+    success "BBR успешно установлен!"
 }
 
 main() {
-    check_bbr
-    install_bbr
+    if ! check_bbr; then
+        return 0
+    fi
 
-    success "Установка завершена!"
+    install_bbr
     read -n 1 -s -r -p "Нажмите любую клавишу для возврата в меню..."
-    "$(dirname "$0")/../../scripts/remnanode/menu.sh"
+    "$(dirname "$0")/menu.sh"
 }
 
 main
