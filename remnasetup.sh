f14cfd8450743113a5b7ebc8e078fa51c6bfea78
Capybara-z <stas89091000@yandex.ru>
2025-05-10 02:42:19 +0300

    Update

--- remnasetup.sh
+++ remnasetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="/opt/remnasetup"
 
 source "${SCRIPT_DIR}/scripts/common/colors.sh"
 source "${SCRIPT_DIR}/scripts/common/functions.sh"
