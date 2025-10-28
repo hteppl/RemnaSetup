c636f4473a21cebca0507976cc957d187a79a619
hteppl <hteppl.dev@gmail.com>
2025-10-28 11:36:49 +0300

    Remove language selection

--- scripts/common/languages.sh
+++ scripts/common/languages.sh
@@ -10,31 +10,7 @@ fi
 
 select_language() {
     if [ -z "$LANGUAGE" ] || [ "$LANGUAGE" != "en" ] && [ "$LANGUAGE" != "ru" ]; then
-        while true; do
-            clear
-            echo -e "${MAGENTA}────────────────────────────────────────────────────────────${RESET}"
-            echo -e "${BOLD_CYAN}Select language / Выберите язык:${RESET}"
-            echo -e "${BLUE}1) English${RESET}"
-            echo -e "${BLUE}2) Русский${RESET}"
-            echo -e "${MAGENTA}────────────────────────────────────────────────────────────${RESET}"
-            read -p "$(echo -e "${BOLD_CYAN}Enter your choice (1-2) / Введите ваш выбор (1-2):${RESET}") " lang_choice
-
-            case $lang_choice in
-                1)
-                    LANGUAGE="en"
-                    break
-                    ;;
-                2)
-                    LANGUAGE="ru"
-                    break
-                    ;;
-                *)
-                    echo -e "${BOLD_RED}Invalid choice. Please select 1 for English or 2 for Russian.${RESET}"
-                    echo -e "${BOLD_RED}Неверный выбор. Пожалуйста, выберите 1 для Английского или 2 для Русского.${RESET}"
-                    sleep 2
-                    ;;
-            esac
-        done
+        LANGUAGE="en"
         echo "$LANGUAGE" > "$LANGUAGE_FILE"
     fi
 }
